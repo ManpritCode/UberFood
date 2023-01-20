cf546e141012bafa4cc808970a62515ce3dd4e20
Manpreet <ms170299singh@gmail.com>
2023-01-20 21:41:35 +0530

    Done SearchBar

---
+++ Components/SearchBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Entypo } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+export default function SearchBar() {
+  return (
+   <View style={{
+    marginTop:15,
+    flexDirection:"row",
+   }}>
+    <GooglePlacesAutocomplete 
+    placeholder='Search'
+    styles={{
+      textInput:{
+        backgroundColor:"#eee",
+        borderRadius:20,
+        fontWeight:"700",
+        marginTop:7,
+      },
+      textInputContainer:{
+        backgroundColor:"#eee",
+        borderRadius:50,
+        flexDirection:"row",
+        alignItems:"center",
+        marginRight:10,
+      }
+
+    }}
+    renderLeftButton={()=>(
+       <View style={{
+        marginLeft:10
+       }}>
+        <Entypo name="location-pin" size={24} color="black" />
+       </View>
+    )}
+    renderRightButton={()=>(
+      <View style={{
+         flexDirection:"row",
+         marginRight:8,
+         backgroundColor:"white",
+         padding:9,
+         borderRadius:30,
+         alignItems:"center",
+      }}>
+        <AntDesign name="clockcircle" size={11} color="black"  style={{marginRight:6}}/>
+      <Text>Search</Text>
+      </View>
+    )}
+    />
+   </View>
+  );
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import React  from 'react'
 import { SafeAreaView, Text, View } from 'react-native'
 import HeaderTab from '../Components/HeaderTab'
+import SearchBar from '../Components/SearchBar'
 
 export default function Home() {
   
@@ -15,6 +16,7 @@ export default function Home() {
       padding:15,
      }}>
      < HeaderTab/>
+     <SearchBar/>
      </View>
      </SafeAreaView>
     )
